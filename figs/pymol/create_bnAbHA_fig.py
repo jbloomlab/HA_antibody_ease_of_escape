cd260c69857d6be43555d1286b9ac6d0fc8dd38b
Juhye Lee <juhyemlee@gmail.com>
2017-06-29 13:25:57 -0700

    add pymol files and start of pymol script to figs directory

---
+++ figs/pymol/create_bnAbHA_fig.py
@@ -0,0 +1,9 @@
+# Pymol script to create figure of HA with antibodies
+
+# LIST OF PDB FILES
+################
+# 1RVX: 1934 H1 HA
+# 3ZTN: FI6v3 in complex with human H1
+# 4GMS: S139/1 in complex with Vic/75 H3
+# 4HLZ: C179 in complex with H2
+################
